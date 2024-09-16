28ddc3e8b22b542e20def648423a0093ca494fcd
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-09-16 23:00:12 +0530

    Change the function into a module.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-function pageLoad() {
+(function pageLoad() {
   const mainContent = document.querySelector("#content");
   const innerdiv = document.createElement("div");
   const heading = document.createElement("h1");
@@ -10,6 +10,4 @@ function pageLoad() {
     "Where good vibes, great drinks, and even better company come together! Whether you're here to unwind after a long day, celebrate with friends, or simply enjoy a night out, we're thrilled to have you with us. Grab a seat, explore our handcrafted cocktails and local brews, and let our friendly staff take care of the rest. At POPs, every night is a celebration, and we can't wait to make your experience unforgettable. Cheers to great times ahead!";
 mainContent.appendChild(heading); 
 mainContent.appendChild(innerdiv);
-}
-
-pageLoad();
+})();
